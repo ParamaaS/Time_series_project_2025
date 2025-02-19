20a0ec10c70a01e1c2527d1d09f52fbc8de1cd4d
ParamaaS <paramaa138@gmail.com>
2025-02-19 23:49:53 +0700

    Be able to extract pixel intensity

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ import pandas as pd
 
 def cal():
     # Example: Extract Average Pixel Intensity from a Sequence of Images
-    image_paths = ["image1.jpg", "image2.jpg", "image3.jpg"]  # Sequential images
-    # time_series_data = []
+    image_paths = ["pictures/snoopy1.png", "pictures/snoopy2.jpg", "pictures/snoopy4.png"] 
+    time_series_data = []
 
-    # for idx, img_path in enumerate(image_paths):
-    #     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)  # Read as grayscale
-    #     avg_intensity = np.mean(img)  # Compute average pixel intensity
-    #     time_series_data.append([idx, avg_intensity])  # Store as time series
-
-    # # Convert to DataFrame
-    # df = pd.DataFrame(time_series_data, columns=['Time', 'Avg_Pixel_Intensity'])
-    # print(df)
+    for idx, img_path in enumerate(image_paths):
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)  # Read as grayscale
+        # avg_intensity = np.mean(img)  # Compute average pixel intensity        
+        df = pd.DataFrame(img)
+        idx+=1
+        print(df)
+        df_1d = df.stack().reset_index(drop=True).to_frame(name="Values")
+        df_1d.to_csv(f"pixel_intensity{idx}.csv",index=False)
 
 
 def main():
-    pass
+    cal()
 
-
-
+if __name__=="__main__":
+    main()
